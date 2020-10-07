a0c7c073eb896985cd17a6122ea674dea3c2848b
MakkaiPeti01 <PetyaPower@gmail.com>
2020-10-07 09:28:46 +0200

    dictionary

--- nyeroszam/Program.cs
+++ nyeroszam/Program.cs
@@ -18,34 +18,35 @@ namespace nyeroszam
              *megnézzük az előfordulást:
              *-ha nincs benne 1, nincs nyertes
              *-ha van benne akkor a legkisebb ertékű a nyertes*/
-            int[] tomb = new int[] { 1, 1, 2, 2, 2, 2, 3, 4, 4 };
-            List<int> lista = new List<int>();
-            List<int> db = new List<int>();
-            foreach (var t in tomb)
-            {
-                if (!lista.Contains(t))
+            int[] tomb = new int[] { 1, 1, 2, 2, 2, 2, 3, 3, 4, 4 };
+            #region regimegoldas
+            /*List<int> lista = new List<int>();
+               List<int> db = new List<int>();
+               foreach (var t in tomb)
+               {
+                   if (!lista.Contains(t))
+                   {
+                       lista.Add(t);
+                   }
+               }
+               lista.Sort();
+               foreach (var sz in lista)
+               {
+                   int mennyi = 0;
+                   foreach (var t in tomb)
+                   {
+                       if (sz == t)
+                       {
+                           mennyi++;
+                       }
+                   }
+                   db.Add(mennyi);
+               }
+               /* for (int i = 0; i < lista.Count; i++)
                 {
-                    lista.Add(t);
-                }
-            }
-            lista.Sort();
-            foreach (var sz in lista)
-            {
-                int mennyi = 0;
-                foreach (var t in tomb)
-                {
-                    if (sz == t)
-                    {
-                        mennyi++;
-                    }
-                }
-                db.Add(mennyi);
-            }
-            /* for (int i = 0; i < lista.Count; i++)
-             {
-                 Console.WriteLine($"{lista[i]} : {db[i]}");
-             }*/
-            int i = 0;
+                    Console.WriteLine($"{lista[i]} : {db[i]}");
+                }*/
+            /*int i = 0;
             while (i < lista.Count && db[i] != 1)
             {
                 i++;
@@ -53,6 +54,31 @@ namespace nyeroszam
             if (i < lista.Count)
             {
                 Console.WriteLine("A nyerő szám {0}", lista[i]);
+            }
+            else
+            {
+                Console.WriteLine("Nincs nyerő szám");
+            }*/
+            #endregion
+            Dictionary<int, int> szamok = new Dictionary<int, int>();
+            foreach (var i in tomb)
+            {
+                if (szamok.ContainsKey(i))
+                {
+                    szamok[i] = szamok[i] + 1;
+                }
+                else
+                {
+                    szamok.Add(i, 1);
+                }
+            }
+            foreach (var i in szamok)
+            {
+                Console.WriteLine(i.Key+ "-->" +i.Value);
+            }
+            if (szamok.ContainsValue(1))
+            {
+                Console.WriteLine("Van nyerő szám");
             }
             else
             {
